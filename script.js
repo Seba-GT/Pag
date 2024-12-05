ccfed98eae72191a3d0248fea34f97e7d6773b5f
Seba-GT <166445615+Seba-GT@users.noreply.github.com>
2024-12-04 22:41:07 -0300

    script.js

--- script.js
+++ script.js
@@ -3,17 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
         navigator.vibrate([500, 300, 500]);
     }
     alert('¡Fuiste hackeado!');
-
-    // Solicitar acceso a la cámara
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function(stream) {
-                var video = document.getElementById('video');
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(function(err) {
-                console.log("Error al acceder a la cámara: " + err);
-            });
-    }
 });
