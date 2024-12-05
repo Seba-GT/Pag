f5f666398de618039553c1f7dd76dcb7136f495e
Seba-GT <166445615+Seba-GT@users.noreply.github.com>
2024-12-04 22:48:37 -0300

    script.js

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
         navigator.vibrate([500, 300, 500]);
     }
     alert('¡Fuiste hackeado!');
+
+    // Cambiar el color de fondo rápidamente
+    setInterval(function() {
+        document.body.style.backgroundColor = getRandomColor();
+    }, 100);
+
+    // Reproducir un sonido
+    var audio = new Audio('https://www.soundjay.com/button/beep-07.wav');
+    audio.play();
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
